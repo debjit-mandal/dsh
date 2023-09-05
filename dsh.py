aaea9c3c9362d5e2f106aa217cbba4fc93a3e588
Debjit Mandal <115482933+debjit-mandal@users.noreply.github.com>
2023-09-05 10:11:59 +0000

    1. **shlex.split()** handles commands like `echo "Hello World"` correctly, ensuring that `"Hello World"` is treated as a single argument. 2. The **current directory** is displayed in the prompt, making it clear where the user currently is. 3. A **built-in 'cd' command** is added to handle directory changes, as subprocess cannot change the directory for the main process.

--- dsh.py
+++ dsh.py
@@ -2,24 +2,39 @@
 
 import subprocess
 import readline
+import os
+import shlex
+
 
 def execute_command(command):
 
     try:
-        result = subprocess.run(command.split(), capture_output=True, text=True)
+        cmd_list = shlex.split(command)
+
+        if cmd_list[0] == "cd":
+            try:
+                path = cmd_list[1] if len(
+                    cmd_list) > 1 else os.path.expanduser("~")
+                os.chdir(path)
+            except OSError as e:
+                print(f"Error changing directory: {e}")
+            return
+
+        result = subprocess.run(cmd_list, capture_output=True, text=True)
         if result.stdout:
             print(result.stdout, end="")
         if result.stderr:
             print(result.stderr, end="")
     except FileNotFoundError:
-        print(f"Command not found: {command.split()[0]}")
+        print(f"Command not found: {cmd_list[0]}")
     except Exception as e:
         print(f"Error executing command: {e}")
 
+
 def dsh():
-    prompt = 'Dsh> '
     while True:
         try:
+            prompt = f"{os.getcwd()} > "
             command = input(prompt).strip()
 
             if not command:
@@ -31,11 +46,10 @@ def dsh():
             execute_command(command)
 
         except KeyboardInterrupt:
-            # Handle Ctrl+C
             print('^C')
         except EOFError:
-            # Handle Ctrl+D
             break
+
 
 if __name__ == "__main__":
     dsh()
