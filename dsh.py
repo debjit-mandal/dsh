b04b94c464a12ed1455be995c1730e57ad1f7e50
Debjit Mandal <115482933+debjit-mandal@users.noreply.github.com>
2023-09-05 15:23:07 +0530

    1. The `subprocess.run` function is used, allowing us to capture both standard output and error output separately, and also provide better error messages. 2. The `readline` module implicitly provides the history mechanism. After executing some commands, the arrow keys can be used (up/down) to cycle through command history. 3. Better error handling is added for situations where the command does not exist or if there's any other exception.

--- dsh.py
+++ dsh.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
 
-import os
+import subprocess
 import readline
+
+def execute_command(command):
+
+    try:
+        result = subprocess.run(command.split(), capture_output=True, text=True)
+        if result.stdout:
+            print(result.stdout, end="")
+        if result.stderr:
+            print(result.stderr, end="")
+    except FileNotFoundError:
+        print(f"Command not found: {command.split()[0]}")
+    except Exception as e:
+        print(f"Error executing command: {e}")
 
 def dsh():
     prompt = 'Dsh> '
     while True:
         try:
-            command = input(prompt)
+            command = input(prompt).strip()
+
+            if not command:
+                continue
 
             if command in ['exit', 'quit']:
                 break
 
-            os.system(command)
+            execute_command(command)
 
         except KeyboardInterrupt:
-
+            # Handle Ctrl+C
             print('^C')
         except EOFError:
+            # Handle Ctrl+D
             break
 
 if __name__ == "__main__":
