f11528ff0ad077cbc5cc86def535bacac0c82b09
Debjit Mandal <115482933+debjit-mandal@users.noreply.github.com>
2023-09-05 09:47:05 +0000

    Initial Commit

--- dsh.py
+++ dsh.py
@@ -7,21 +7,17 @@ def dsh():
     prompt = 'Dsh> '
     while True:
         try:
-            # Read input from the user
             command = input(prompt)
 
-            # Exit the shell if the command is 'exit' or 'quit'
             if command in ['exit', 'quit']:
                 break
 
-            # Execute the command
             os.system(command)
 
         except KeyboardInterrupt:
-            # Handle Ctrl+C
+
             print('^C')
         except EOFError:
-            # Handle Ctrl+D
             break
 
 if __name__ == "__main__":
