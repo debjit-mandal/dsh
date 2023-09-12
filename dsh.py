dbe3423ad0302131f85e7cb8ef934e50954f7248
Debjit Mandal <debjitmandal8925@gmail.com>
2023-09-12 12:47:36 +0530

    SIGINT Handler, Command history persistence added, tab completion improved

--- dsh.py
+++ dsh.py
@@ -4,8 +4,24 @@ import subprocess
 import readline
 import os
 import shlex
+import signal
 
-readline.parse_and_bind("tab: complete")
+HISTORY_FILE = os.path.expanduser("~/.customshell_history")
+HISTORY_LENGTH = 1000
+
+def setup_readline():
+    readline.parse_and_bind("tab: complete")
+    
+    if os.path.exists(HISTORY_FILE):
+        readline.read_history_file(HISTORY_FILE)
+
+    readline.set_history_length(HISTORY_LENGTH)
+
+def complete(text, state):
+    builtins = ['cd', 'help', 'exit', 'quit']
+    return ([
+        cmd for cmd in builtins if cmd.startswith(text)
+    ] + [filename for filename in os.listdir() if filename.startswith(text)])[state]
 
 def execute_command(command):
     cmd_list = shlex.split(command)
@@ -45,26 +61,30 @@ def run_system_command(cmd_list):
     except Exception as e:
         print(f"Error executing command: {e}")
 
-def dsh():
+def sigint_handler(signal, frame):
+    print("\n^C")
+    print(f"{os.getcwd()} > ", end="")
+    readline.redisplay()
+
+def custom_shell():
+    setup_readline()
+    readline.set_completer(complete)
+    signal.signal(signal.SIGINT, sigint_handler)
+
     while True:
         try:
             prompt = f"{os.getcwd()} > "
             command = input(prompt).strip()
-
             if not command:
                 continue
-
             if command in ['exit', 'quit']:
+                readline.write_history_file(HISTORY_FILE)
                 break
-
             execute_command(command)
-
-        except KeyboardInterrupt:
-            print('^C')
-        except EOFError:
+        except EOFError:  
+            readline.write_history_file(HISTORY_FILE)
             break
 
 if __name__ == "__main__":
-    dsh()
-
+    custom_shell()
 
