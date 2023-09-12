b0b0475e8f8cd7a89c8d126efb976aa9bb458d62
Debjit Mandal <debjitmandal8925@gmail.com>
2023-09-12 13:03:25 +0530

    Organized the functionalities into a  class for cleaner organization and potential expansion.

--- dsh.py
+++ dsh.py
@@ -6,85 +6,97 @@ import os
 import shlex
 import signal
 
-HISTORY_FILE = os.path.expanduser("~/.customshell_history")
+HISTORY_FILE = os.path.expanduser("~/.dsh_history")
 HISTORY_LENGTH = 1000
 
-def setup_readline():
-    readline.parse_and_bind("tab: complete")
-    
-    if os.path.exists(HISTORY_FILE):
-        readline.read_history_file(HISTORY_FILE)
+class dsh:
 
-    readline.set_history_length(HISTORY_LENGTH)
+    def __init__(self):
+        # self.aliases = {
+            # 'ls': 'ls --color=auto',
+            # 'll': 'ls -la'
+        # }
+        self.setup_readline()
+        self.setup_signals()
 
-def complete(text, state):
-    builtins = ['cd', 'help', 'exit', 'quit']
-    return ([
-        cmd for cmd in builtins if cmd.startswith(text)
-    ] + [filename for filename in os.listdir() if filename.startswith(text)])[state]
+    def setup_readline(self):
+        readline.parse_and_bind("tab: complete")
+        if os.path.exists(HISTORY_FILE):
+            readline.read_history_file(HISTORY_FILE)
+        readline.set_history_length(HISTORY_LENGTH)
+        readline.set_completer(self.complete)
 
-def execute_command(command):
-    cmd_list = shlex.split(command)
+    def setup_signals(self):
+        signal.signal(signal.SIGINT, self.sigint_handler)
 
-    if cmd_list[0] == "cd":
-        handle_cd_command(cmd_list)
-    elif cmd_list[0] == "help":
-        display_help()
-    else:
-        run_system_command(cmd_list)
+    def complete(self, text, state):
+        builtins = ['cd', 'help', 'exit', 'quit']
+        return ([
+            cmd for cmd in builtins if cmd.startswith(text)
+        ] + [filename for filename in os.listdir() if filename.startswith(text)])[state]
 
-def handle_cd_command(cmd_list):
-    try:
-        path = cmd_list[1] if len(cmd_list) > 1 else os.path.expanduser("~")
-        os.chdir(path)
-    except OSError as e:
-        print(f"Error changing directory: {e}")
+    def execute_command(self, command):
+        cmd_list = shlex.split(command)
+        # cmd = self.aliases.get(cmd_list[0], cmd_list[0])
+        # cmd_list[0] = cmd
 
-def display_help():
-    help_text = """
-    CustomShell Built-in Commands:
+        if cmd_list[0] == "cd":
+            self.handle_cd_command(cmd_list)
+        elif cmd_list[0] == "help":
+            self.display_help()
+        else:
+            self.run_system_command(cmd_list)
+
+    def handle_cd_command(self, cmd_list):
+        try:
+            path = cmd_list[1] if len(cmd_list) > 1 else os.getenv("HOME")
+            os.chdir(path)
+        except OSError as e:
+            print(f"Error changing directory: {e}")
+
+    def display_help(self):
+        help_text = """
+    dsh Built-in Commands:
     cd [directory]   - Change directory.
     help             - Display this help message.
     exit / quit      - Exit the shell.
     """
-    print(help_text)
+        print(help_text)
 
-def run_system_command(cmd_list):
-    try:
-        result = subprocess.run(cmd_list, capture_output=True, text=True)
-        if result.stdout:
-            print(result.stdout, end="")
-        if result.stderr:
-            print(result.stderr, end="")
-    except FileNotFoundError:
-        print(f"Command not found: {cmd_list[0]}")
-    except Exception as e:
-        print(f"Error executing command: {e}")
-
-def sigint_handler(signal, frame):
-    print("\n^C")
-    print(f"{os.getcwd()} > ", end="")
-    readline.redisplay()
-
-def custom_shell():
-    setup_readline()
-    readline.set_completer(complete)
-    signal.signal(signal.SIGINT, sigint_handler)
-
-    while True:
+    def run_system_command(self, cmd_list):
         try:
-            prompt = f"{os.getcwd()} > "
-            command = input(prompt).strip()
-            if not command:
-                continue
-            if command in ['exit', 'quit']:
+            result = subprocess.run(cmd_list, capture_output=True, text=True)
+            if result.stdout:
+                print(result.stdout, end="")
+            if result.stderr:
+                print(result.stderr, end="")
+        except FileNotFoundError:
+            print(f"Command not found: {cmd_list[0]}")
+        except Exception as e:
+            print(f"Error executing command: {e}")
+
+    def sigint_handler(self, signal, frame):
+        print("\n^C")
+        print(self.prompt(), end="")
+        readline.redisplay()
+
+    def prompt(self):
+        return f"{os.getcwd()} > "
+
+    def loop(self):
+        while True:
+            try:
+                command = input(self.prompt()).strip()
+                if not command:
+                    continue
+                if command in ['exit', 'quit']:
+                    readline.write_history_file(HISTORY_FILE)
+                    break
+                self.execute_command(command)
+            except EOFError:  
                 readline.write_history_file(HISTORY_FILE)
                 break
-            execute_command(command)
-        except EOFError:  
-            readline.write_history_file(HISTORY_FILE)
-            break
 
 if __name__ == "__main__":
-    custom_shell()
-
+    shell = dsh()
+    shell.loop()
